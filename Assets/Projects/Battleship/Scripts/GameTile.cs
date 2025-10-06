1ffacd45911095654e5846d8877bf489c3d3327b
Anton <a.e.lindkvist@gmail.com>
2025-10-07 01:26:41 +0200

    Additional visuals

--- Assets/Projects/Battleship/Scripts/GameTile.cs
+++ Assets/Projects/Battleship/Scripts/GameTile.cs
@@ -1,22 +1,60 @@
-using UnityEditor;
 using UnityEngine;
 
 namespace Battleship
 {
     public class GameTile : MonoBehaviour
     {
+        [SerializeField] private float _oscillationSpeed = 0.05f;
+        [SerializeField] private float _oscillationHeight = 0.1f;
+        [SerializeField] private float _lerpSpeed = 3f;
+        [SerializeField] private float _punchResetLerpSpeed = 3f;
+        private Vector3 _startPos = Vector3.zero;
+        [SerializeField]
+        private Vector3 _punchOffset = Vector3.zero;
         private int _x, _y;
+        private float _offset = 0f;
 
         public void Init(float scale, int x, int y)
         {
             _x = x;
             _y = y;
             transform.localScale = Vector3.one * scale;
+            _startPos = transform.localPosition;
+            _offset = 0.1f * x;
         }
 
         public void Marked()
         {
             GetComponent<SpriteRenderer>().color = new Color(0.8f, 0.8f, 0.8f);
+        }
+
+        public void UpdateTile(Sprite sprite, float pieceRotation)
+        {
+            if (sprite != null)
+            {
+                if (TryGetComponent(out SpriteRenderer comp))
+                {
+                    comp.sprite = sprite;
+                    
+                    transform.localRotation = Quaternion.Euler(0f, 0, pieceRotation);
+                }
+            }
+        }
+
+        public void PunchTile(Vector3 direction, float strength)
+        {
+            _punchOffset = direction * strength;
+        }
+
+        private void Update()
+        {
+            if (_punchOffset != Vector3.zero)
+            {
+                _punchOffset = Vector3.Lerp(_punchOffset, Vector3.zero, Time.deltaTime * _punchResetLerpSpeed);
+            }
+
+            var targetPosition = _startPos + _punchOffset + Vector3.up * Mathf.PingPong((Time.time + _offset) * _oscillationSpeed, _oscillationHeight);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * _lerpSpeed);
         }
 
         //private void OnDrawGizmos()
--- Assets/Projects/Battleship/Scripts/PlayerVisuals.cs
+++ Assets/Projects/Battleship/Scripts/PlayerVisuals.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,36 +8,90 @@ namespace Battleship
     public class PlayerVisuals : MonoBehaviour
     {
         private int _playerID = 0;
-        Dictionary<Vector2Int, GameTile> _coordinateDisplay = new();
+        Dictionary<Vector2Int, GameTile> _tileDictionary = new();
 
         [SerializeField] private GameTile _gameTilePrefab = null;
         [SerializeField] private Transform _boardHolder = null;
         [SerializeField] private SpriteRenderer _expressionRenderer = null;
         [SerializeField] private List<Sprite> _playerExpressions = new List<Sprite>();
+        [SerializeField] private List<Sprite> _shipSprites = new List<Sprite>();
         [SerializeField] private GameObject _explosionVFX = null;
         [SerializeField] private GameObject _waterVFX = null;
+        [SerializeField] private GameObject _fireVFX = null;
         [SerializeField] private TextMeshProUGUI _nameLabel = null;
         [SerializeField] private Image _healthBarFill = null;
+        [SerializeField] private float _punchStrength = 3f;
 
         private float _gridCellScale = 1f;
         private float _gridSpacing = 1f;
         public void SetupReplayVisuals(BattleshipReplay battleshipReplay, GameData gameData, int playerID, float gridCellScale, float gridSpacing)
         {
-            _nameLabel.text = playerID == 0 ? gameData.User0.UserName : gameData.User1.UserName;
-            _playerID = playerID == 0 ? gameData.User0.PlayerID : gameData.User1.PlayerID;
+            var user = playerID == 0 ? gameData.User0 : gameData.User1;
+            _playerID = user.PlayerID;
+            _nameLabel.text = user.UserName;
             battleshipReplay.TurnEvent += OnTurnEvent;
             _gridCellScale = gridCellScale;
             _gridSpacing = gridSpacing;
 
-            Vector2Int gridSize = new(gameData.User0.Grid.GetLength(1), gameData.User0.Grid.GetLength(0));
+            Vector2Int gridSize = new(user.Grid.GetLength(1), user.Grid.GetLength(0));
             for (int x = 0; x < gridSize.x; x++)
             {
                 for (int y = 0; y < gridSize.y; y++)
                 {
                     var obj = Instantiate(_gameTilePrefab, _boardHolder.transform);
+                    _tileDictionary.TryAdd(new Vector2Int(x, y), obj);
                     obj.transform.localPosition = new Vector2(x * gridCellScale + x * gridSpacing, y * gridCellScale + y * gridSpacing);
                     obj.Init(gridCellScale, x, y);
-                    _coordinateDisplay.TryAdd(new Vector2Int(x, y), obj);
+                }
+            }
+
+            ApplyShipSprites(user);
+        }
+
+        private void ApplyShipSprites(UserData user)
+        {
+            foreach (var shipData in user.ShipData)
+            {
+                var start = shipData.ShipCoordinates[0];
+                var end = shipData.ShipCoordinates[shipData.ShipCoordinates.Count - 1];
+                var dir = end - start;
+                float pieceRotation = 0f;
+                if (dir.x != 0)
+                {
+                    if (dir.x > 0)
+                        pieceRotation = 0f;
+                    else
+                        pieceRotation = 180f;
+                }
+                else
+                {
+                    if (dir.y > 0)
+                        pieceRotation = 90f;
+                    else
+                        pieceRotation = 270f;
+                }
+
+                if (shipData.ShipCoordinates.Count == 1)
+                {
+                    _tileDictionary[shipData.ShipCoordinates[0]].UpdateTile(_shipSprites[3], 0f);
+                }
+                else
+                {
+                    foreach (var shipCoordinate in shipData.ShipCoordinates)
+                    {
+                        if (shipCoordinate == start)
+                        {
+                            _tileDictionary[shipCoordinate].UpdateTile(_shipSprites[0], pieceRotation);
+                        }
+                        else if (shipCoordinate == end)
+                        {
+                            _tileDictionary[shipCoordinate].UpdateTile(_shipSprites[2], pieceRotation);
+                        }
+                        else
+                        {
+                            _tileDictionary[shipCoordinate].UpdateTile(_shipSprites[1], pieceRotation);
+                        }
+                    }
                 }
             }
         }
@@ -51,21 +104,36 @@ namespace Battleship
                 {
                     _expressionRenderer.sprite = _playerExpressions[2]; // smug
                 }
-                else
+                else if (turn.ShipCellsRemaining != 1)
                 {
-
+                    _expressionRenderer.sprite = _playerExpressions[3]; // look
                 }
             }
             else
             {
-                _healthBarFill.fillAmount = (1f / 13f) * turn.ShipCellsRemaining;
-                _coordinateDisplay[turn.FireTarget].Marked();
+                _healthBarFill.fillAmount = (1f / 13f) * turn.ShipCellsRemaining; // magical number, bad
+                _tileDictionary[turn.FireTarget].Marked();
+
+                var targetPosition = _tileDictionary[turn.FireTarget].transform.position;
+                foreach (var entry in _tileDictionary)
+                {
+                    var dir = (entry.Value.transform.position - targetPosition).normalized;
+                    entry.Value.PunchTile(dir, _punchStrength - Mathf.Clamp((entry.Value.transform.position - targetPosition).sqrMagnitude, 0, _punchStrength));
+                }
+
 
                 if (turn.Hit)
                 {
-                    _expressionRenderer.sprite = _playerExpressions[1]; // sad
+                    if (turn.ShipCellsRemaining == 1)
+                    {
+                        _expressionRenderer.sprite = _playerExpressions[3]; // look
+                    }
+                    else
+                    {
+                        _expressionRenderer.sprite = _playerExpressions[1]; // sad
+                    }
                 }
-                else
+                else if (turn.ShipCellsRemaining != 1)
                 {
                     _expressionRenderer.sprite = _playerExpressions[0]; // content
                 }
@@ -78,12 +146,14 @@ namespace Battleship
         {
             if (hit)
             {
-                var obj = Instantiate(_explosionVFX, _coordinateDisplay[coordinate].transform.position, Quaternion.identity);
-                obj.transform.localScale = Vector3.one * _gridCellScale;
+                var explosion = Instantiate(_explosionVFX, _tileDictionary[coordinate].transform.position, Quaternion.identity);
+                explosion.transform.localScale = Vector3.one * _gridCellScale;
+                var fire = Instantiate(_fireVFX, _tileDictionary[coordinate].transform.position, Quaternion.identity);
+                fire.transform.localScale = Vector3.one * _gridCellScale;
             }
             else
             {
-                var obj = Instantiate(_waterVFX, _coordinateDisplay[coordinate].transform.position, Quaternion.identity);
+                var obj = Instantiate(_waterVFX, _tileDictionary[coordinate].transform.position, Quaternion.identity);
                 obj.transform.localScale = Vector3.one * _gridCellScale;
             }
         }
